ba68098b1d8a79d05bfac6eddaa7bd5d4cb9306e
Markus Stenberg <markus.stenberg@iki.fi>
2024-10-03 10:22:54 +0300

    Initial Commit

---
+++ swift-go-test/swift_go_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_go_testApp.swift
+//  swift-go-test
+//
+//  Created by Markus Stenberg on 3.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_go_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-go-testTests/swift_go_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_go_testTests.swift
+//  swift-go-testTests
+//
+//  Created by Markus Stenberg on 3.10.2024.
+//
+
+import Testing
+@testable import swift_go_test
+
+struct swift_go_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
